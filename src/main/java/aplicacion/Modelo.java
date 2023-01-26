980c961a738a88eea39636aced1bfc780367b2e2
Angel <a.martinez258@usp.ceu.es>
2023-01-26 20:48:46 +0000

    Castellano + Comentarios

--- src/main/java/aplicacion/Modelo.java
+++ src/main/java/aplicacion/Modelo.java
@@ -1,8 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Copyright 2023 Ángel Martínez Espina
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package aplicacion;
 
 import java.io.BufferedReader;
@@ -25,7 +32,6 @@ import java.util.logging.Logger;
 
 
 /**
- *
  * @author angel
  */
 
@@ -36,23 +42,22 @@ import java.util.logging.Logger;
  * https://stackoverflow.com/questions/12953958/how-to-create-an-arff-file-from-an-array-in-java
  */
 public class Modelo {
- // Los Atributos Sexo y HeartDisease son de tipo Nominal porque
-// representa un valor tomado de un conjunto discreto de valores posibles
-List<String> AtributosSexo = Arrays.asList("M","F");
-List<String> AtributosHeartDisease = Arrays.asList("0","1");
+    // Los Atributos Sexo y HeartDisease son de tipo Nominal porque
+    // representa un valor tomado de un conjunto discreto de valores posibles
+    List<String> AtributosSexo = Arrays.asList("M","F");
+    List<String> AtributosHeartDisease = Arrays.asList("0","1");
 
-// Declaramos Atributos de Tipo Numerico
-Attribute Age = new Attribute("Age");
-Attribute RestingBP = new Attribute("RestingBP");
-Attribute Cholesterol = new Attribute("Cholesterol");
-Attribute FastingBS = new Attribute("FastingBS");
-Attribute MaxHR = new Attribute("MaxHR");
-Attribute HeartDisease = new Attribute("HeartDisease",AtributosHeartDisease);
-Attribute Sex = new Attribute("Sex",AtributosSexo);
+    // Declaramos Atributos de Tipo Numerico
+    Attribute Age = new Attribute("Age");
+    Attribute RestingBP = new Attribute("RestingBP");
+    Attribute Cholesterol = new Attribute("Cholesterol");
+    Attribute FastingBS = new Attribute("FastingBS");
+    Attribute MaxHR = new Attribute("MaxHR");
+    Attribute HeartDisease = new Attribute("HeartDisease",AtributosHeartDisease);
+    Attribute Sex = new Attribute("Sex",AtributosSexo);
 
-// ArrayList que va a contener todos los atributos
-ArrayList<Attribute> atributos = new ArrayList<>(Arrays.asList(Age,Sex,RestingBP,Cholesterol,FastingBS,MaxHR,HeartDisease));
-
+    // ArrayList que va a contener todos los atributos
+    ArrayList<Attribute> atributos = new ArrayList<>(Arrays.asList(Age,Sex,RestingBP,Cholesterol,FastingBS,MaxHR,HeartDisease));
 
     public void aprenderModelo(){
         try{
@@ -75,15 +80,29 @@ ArrayList<Attribute> atributos = new ArrayList<>(Arrays.asList(Age,Sex,RestingBP
             }
     }
 
-    public String generarInstanciaConsulta(Integer ValorAge, String ValorSexo,Integer ValorRestingBP, Integer ValorCholesterol, Integer ValorFastingBS, Integer ValorMaxHR){
+    /**
+     * Almacena los valores de la consulta en una instancia. 
+     * @param edad: Edad del sujeto
+     * @param sexo: Sexo del sujeto
+     * @param presArtReposo: Presión Arterial Reposo del sujeto. [mm Hg]
+     * @param colesterol: Colestero del sujeto
+     * @param glucemiaAyunas: Glucemia del Sujeto ==> 1 if > 120 mg/dl | 0 en el resto de casos 
+     * @param frecuenciaCardiacaMax: Valor entre 60 y 202
+     * @return Resultado de la Clasificación. 0 ==> Paciente Común. 1==> Insuficiencia Cardíaca 
+     */
+
+    public String generarInstanciaConsulta(Integer edad, String sexo,Integer presArtReposo, Integer colesterol, Integer glucemiaAyunas, Integer frecuenciaCardiacaMax){
+        // Creamos una Instances. ( Representación en memoria de una colección de ejemplos)
        Instances InstancesConsulta = new Instances("InstancesConsulta",atributos,atributos.size());
+       // Creamos una Instance. (Almacena los valores de un ejemplo). Los valores tomarán los valores introducidos por el usuario.
         Instance instancia = new DenseInstance(atributos.size());
-        instancia.setValue(Age,ValorAge);
-        instancia.setValue(Sex,ValorSexo);
-        instancia.setValue(RestingBP,ValorRestingBP);
-        instancia.setValue(Cholesterol,ValorCholesterol);
-        instancia.setValue(FastingBS,ValorFastingBS);
-        instancia.setValue(MaxHR,ValorMaxHR);
+        instancia.setValue(Age,edad);
+        instancia.setValue(Sex,sexo);
+        instancia.setValue(RestingBP,presArtReposo);
+        instancia.setValue(Cholesterol,colesterol);
+        instancia.setValue(FastingBS,glucemiaAyunas);
+        instancia.setValue(MaxHR,frecuenciaCardiacaMax);
+        // Añadimos la Instance a la Instances. 
         InstancesConsulta.add(instancia);
         // Establecemos el atributo clase. 
         InstancesConsulta.setClassIndex(InstancesConsulta.numAttributes()-1);
@@ -95,9 +114,6 @@ ArrayList<Attribute> atributos = new ArrayList<>(Arrays.asList(Age,Sex,RestingBP
             }catch (Exception ex) {
             Logger.getLogger(Modelo.class.getName()).log(Level.SEVERE, null, ex);
             return "Error al intentar leer el modelo";
-        }
-
-
-        
+        }        
     }
 }
--- src/main/java/rest/APIController.java
+++ src/main/java/rest/APIController.java
@@ -1,30 +1,46 @@
+/*Copyright 2023 Ángel Martínez Espina
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 package rest;
 
 import aplicacion.*;
-
-
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author angel
+ */
+
+
 @RestController
 
 /*
  * Ejemplos de Consultas: 
- * localhost:8080/aprenderModelo?ValorAge=40&ValorSexo=M&ValorRestingBP=140&ValorCholesterol=289&ValorFastingBS=0&ValorMaxHR=172
- * localhost:8080/aprenderModelo?ValorAge=49&ValorSexo=F&ValorRestingBP=160&ValorCholesterol=180&ValorFastingBS=0&ValorMaxHR=156
+ * Resultado 0 ==> localhost:8080/aprenderModelo?edad=40&sexo=M&presArtReposo=140&colesterol=289&glucemiaAyunas=0&frecuenciaCardiacaMax=172
+ * Resultado 1==> localhost:8080/aprenderModelo?edad=49&sexo=F&presArtReposo=160&colesterol=180&glucemiaAyunas=0&frecuenciaCardiacaMax=156
  */
 public class APIController {
 	
 	@GetMapping("/aprenderModelo")
-	public String  modelo(@RequestParam Integer ValorAge, String ValorSexo,Integer ValorRestingBP, Integer ValorCholesterol, Integer ValorFastingBS, Integer ValorMaxHR){
+
+	public String  modelo(@RequestParam Integer edad, String sexo,Integer presArtReposo, Integer colesterol, Integer glucemiaAyunas, Integer frecuenciaCardiacaMax){
 		Modelo modelo = new Modelo();
 		modelo.aprenderModelo();
-		return("Al aplicar RandomForest a los datos aportados, el resultado es " + modelo.generarInstanciaConsulta(ValorAge,ValorSexo,ValorRestingBP,ValorCholesterol,ValorFastingBS,ValorMaxHR));
+		return("Al aplicar RandomForest a los datos aportados, el resultado es " + modelo.generarInstanciaConsulta(edad,sexo,presArtReposo,colesterol,glucemiaAyunas,frecuenciaCardiacaMax));
 			
-		
-		
+
 	}
 
 }
