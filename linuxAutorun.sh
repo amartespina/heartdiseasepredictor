9c5042de7d7942e8b57212d804052ff68169d06a
Angel Martinez <a.martinez258@usp.ceu.es>
2023-06-05 10:04:21 +0200

    05/06

--- linuxAutorun.sh
+++ linuxAutorun.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/bash
-
-git clone https://github.com/amartespina/heartfailurepredictor.git
-cd heartfailurepredictor
 mvn package
+cd heartfailurepredictor
 cp -r src ./target
 cd target
 sudo java -jar heartfailurepredictor-3.0.0.jar
